846056082c3fc9abbfda346ab99665f78929c106
manjitsaha <manjitsaha01@gmail.com>
2025-08-18 10:15:27 +0530

    changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -76,10 +76,7 @@ const authUser = asyncHandler(async (req, res) => {
     }
 
     res.status(200).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
+      user,
       token: generateToken(user._id),
     });
   } else {
